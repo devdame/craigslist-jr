64efb0f824de4aa9f303e3a0da54155ad503b99a
wisconsinpoet <dandydanny@gmail.com>
2014-02-18 21:24:16 -0600

    updated database and finished update page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 require 'faker'
 
-10.times do
+cats = ["Little Teacups", "Glasses", "Motorcycles", "Tiny Corvettes", "Dresses", "Leather Jackets", "Suspenders", "Snout Powder", "Paw Decals", "Brass Knuckles"]
+
+(0..9).to_a.each do |i|
   Category.create({
-    name: Faker::Company.name,
+    name: cats[i],
     description: Faker::Company.catch_phrase
   })
 end
@@ -10,7 +12,7 @@ end
 Category.all.each do |cat|
 	10.times do
 	  Post.create({
-	    name: Faker::Company.name,
+	    name: Faker::Company.catch_phrase,
 	    description: Faker::Lorem.paragraph,
 	    cost: rand(100..1000),
 	    email: Faker::Internet.email,
